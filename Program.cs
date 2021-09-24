1f68b1f4b6260a55bec0c1009601bc75de818ab0
mariaclaramc <stocklermah@gmail.com>
2021-09-24 14:04:07 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace AuladeFixação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //exec 01 ---------------------- primeira lista
+            //int a = 123;
+            //Console.WriteLine(a);
+
+            //exec 02 ----------------------
+            //int a;
+            //int b;
+            //int c;
+
+            //a = int.Parse(Console.ReadLine());
+            //b = int.Parse(Console.ReadLine());
+            //c = int.Parse(Console.ReadLine());
+
+            //exec 03 ----------------------
+            //int a;
+
+            //a = int.Parse(Console.ReadLine());
+            //Console.WriteLine(a*a);
+
+            //exec 04 ----------------------
+            //double dolar;
+            //double real;
+            //int notas;
+            //double valorDaCotacao;
+
+            //Console.WriteLine("Olá querido usuario, sou eu quem vai lhe ajudar a conseguir saber quanto de dinheiro você possui");
+            //Console.WriteLine("Primeiro você deve informar qual é a atual cotação do dolar");
+            //valorDaCotacao = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Obrigado, agora já sei qual valores temos.");
+            //Console.WriteLine("Quantos dolares você possui atualmente?");
+            //dolar = double.Parse(Console.ReadLine());
+            //Console.WriteLine("E quantas notas você tem?");
+            //notas = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Obrigado mais uma vez, agora vou te mostrar quantos reais você tem!");
+            //real = dolar * valorDaCotacao;
+            //Console.WriteLine("Você possui "+ real + " reais");
+
+            //exec 05 ----------------------
+            //double dolar;
+            //double real;
+            //int notas;
+            //double valorDaCotacao;
+
+            //Console.WriteLine("Olá querido usuario, sou eu quem vai lhe ajudar a conseguir saber quanto de dinheiro você possui");
+            //Console.WriteLine("Primeiro você deve informar qual é a atual cotação do dolar");
+            //valorDaCotacao = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Obrigado, agora já sei qual valores temos.");
+            //Console.WriteLine("Quantos reais você possui atualmente?");
+            //real = double.Parse(Console.ReadLine());
+            //Console.WriteLine("E quantas notas você tem?");
+            //notas = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Obrigado mais uma vez, agora vou te mostrar quantos reais você tem!");
+            //dolar = real / valorDaCotacao;
+            //Console.WriteLine("Você possui " + dolar + " dolares");
+
+            //exec 06 ----------------------
+            //double valordaCompra;
+            //double valorAVista;
+            //double valorAPrazo;
+
+            //Console.WriteLine("Qual foi o valor da sua compra?");
+            //valordaCompra = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Aqui estão os valores e seus modos de pagamento");
+            //Console.WriteLine("Pagamento a vista");
+            //valorAVista = valordaCompra - (valordaCompra * 0.08);
+            //Console.WriteLine(valorAVista);
+            //Console.WriteLine("Pagamento a prazo");
+            //valorAPrazo = valordaCompra / 3;
+            //Console.WriteLine("Você pagará 3 parcelas de " + (valorAPrazo + (valorAPrazo * 0.15)) + " reais");
+
+            //exec 07 ----------------------
+            //int a;
+            //int b;
+            //int c;
+
+            //Console.WriteLine("Escreva dois valores");
+            //a = int.Parse(Console.ReadLine());
+            //b = int.Parse(Console.ReadLine());
+
+            //c = a;
+            //a = b;
+            //b = c;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+        }
+    }
+}
